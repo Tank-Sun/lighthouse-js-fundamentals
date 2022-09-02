0daf980ab3574a0feb0abf2ecff2eb35daf3249f
Yongjia Sun <thomastank0926@gmail.com>
2022-09-02 22:09:14 +0000

    new changes

--- conditionals.js
+++ conditionals.js
@@ -22,7 +22,38 @@ if (temperature < -40 || temperature > 40){
 }*/
 
 
-let raining = false;
+/*let raining = false;
 if(!raining){
   console.log("Leave your umbrella at home!");
+}*/
+
+
+/*if (age < 13){
+  console.log("Elementary School");
+}else if (age >= 13 && age <= 18){
+  console.log("Secondary School");
+}else{
+  console.log("Lighthouse Labs");
 }
+
+const whichSchool  = function (age) {
+  if (age < 13){
+    console.log("Elementary School");
+  }else if (age >= 13 && age <= 18){
+    console.log("Secondary School");
+  }else{
+    console.log("Lighthouse Labs");
+  }
+}*/
+
+const whichSchool  = function (age) {
+  if (age < 13){
+    return "Elementary School";
+  }else if (age >= 13 && age <= 18){
+    return "Secondary School";
+  }else{
+    return "Lighthouse Labs";
+  }
+}
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
